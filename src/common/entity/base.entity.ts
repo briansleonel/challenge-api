c67ae8a43a95e568ccd1dd970225bdc42e89491f
briansleonel <gonzalezbrianleonel76@gmail.com>
2024-05-13 20:59:43 -0300

    feat: add packages and implement common files

---
+++ src/common/entity/base.entity.ts
@@ -0,0 +1,15 @@
+import {
+  CreateDateColumn,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+export class BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: string;
+  @CreateDateColumn({ type: 'timestamp', name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: 'timestamp', name: 'updated_at' })
+  updatedAt: Date;
+}
