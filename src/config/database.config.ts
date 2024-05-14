42d8cc149b70b8bcc57737f6e724a495536b0d6a
briansleonel <gonzalezbrianleonel76@gmail.com>
2024-05-13 21:57:26 -0300

    feat: add database config

---
+++ src/config/database.config.ts
@@ -0,0 +1,9 @@
+import { registerAs } from '@nestjs/config';
+
+export default registerAs('database', () => ({
+  database: process.env.DB_NAME,
+  port: parseInt(process.env.DB_PORT) || 5432,
+  password: process.env.DB_PASSWORD,
+  username: process.env.DB_USER,
+  host: process.env.DB_HOSTNAME,
+}));
